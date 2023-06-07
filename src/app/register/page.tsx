a056b224872d72b9f959627c5b8336443bfaa0d1
ismail anajar <omaranajar@gmail.com>
2023-06-07 19:36:17 +0100

    add register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import {
+  FieldValues,
+  useForm,
+} from 'react-hook-form';
+import { z } from 'zod';
+
+import Button from '@/components/atoms/button';
+import Heading from '@/components/atoms/heading';
+import Input from '@/components/atoms/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+export default function Register() {
+
+  const schema = z.object({
+    name: z.string().min(3),
+    email: z.string().email(),
+    password: z.string()
+  });
+  const {control, handleSubmit,  setError} = useForm({resolver: zodResolver(schema)});
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+    
+  }
+
+  //  useEffect(() => {
+  //   if (error?.errors) {
+      
+  //     errorHandling({error, setError})
+  //   }
+  // }, [error])
+
+  return (
+    <section>
+      <Heading className='mb-12' >Welcome To Goaluin</Heading>
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4 items-center'>
+        <Input name='name' type='text' placeholder='name' control={control}/>
+        <Input name='email' type='email' placeholder='john@example.com' control={control}/>
+        <Input name='password' type='password' placeholder='password' control={control}/>
+        <Button type='submit'>Login</Button>
+      </form>
+    </section>
+  )
+}
